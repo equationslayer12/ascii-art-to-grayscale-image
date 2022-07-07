38f3ea9f2dc15531777502d4891d57b40904f86e
equationslayer12 <roy0708usa@gmail.com>
2022-07-07 14:53:39 +0300

    cleaned code

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
 from PIL import Image
 
-gray_scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-length = len(gray_scale)
+ASCII_ART_FILE_NAME = "art.txt"
+NEW_IMAGE_NAME = "new_image.png"
+
+CHARACTER_REPRESENTATION_IN_GRAYSCALE = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+CHARACTER_REPRESENTATION_IN_GRAYSCALE_LENGTH = len(CHARACTER_REPRESENTATION_IN_GRAYSCALE)
 
 
 def letter_to_rgb(letter):
     try:
-        value = int(gray_scale.index(letter) / length * 255)
+        color = int(CHARACTER_REPRESENTATION_IN_GRAYSCALE.index(letter) / CHARACTER_REPRESENTATION_IN_GRAYSCALE_LENGTH * 255)
     except ValueError:
-        value = 255
+        color = 255
 
-    return value, value, value
+    return color, color, color
 
 
-with open("art.txt", "r") as file:
-    WIDTH = len(max(file, key=len))
+def get_longest_line_from_file(file):
+    width = len(max(file, key=len))
     file.seek(0)
-    HEIGHT = 0
+    return width
 
-    pixels = []
-    for line in file:
+
+def count_file_newlines(file):
+    height = len(file.readlines())
+    file.seek(0)
+    return height
+
+
+def convert_ascii_art_to_image_data(ascii_art_file, image_width):
+    image_data = []
+    for line in ascii_art_file:
         for x in line:
-            pixels.append(
+            image_data.append(
                 letter_to_rgb(x)
             )
 
-        for x in range(WIDTH - len(line)):
-            pixels.append((255, 255, 255))
+        fill_rest_of_line_with_white(image_data, line, image_width)
+    return image_data
 
-        HEIGHT += 1
 
-    image = Image.new('RGB', (WIDTH, HEIGHT))
-    image.putdata(pixels)
-    image.save('new_image.png')
+def fill_rest_of_line_with_white(image_data, line, image_width):
+    for x in range(image_width - len(line)):
+        image_data.append((255, 255, 255))
+
+
+def get_file_dimentions(ascii_art_file):
+    image_width = get_longest_line_from_file(ascii_art_file)
+    image_height = count_file_newlines(ascii_art_file)
+    return image_width, image_height
+
+
+def save_image(image):
+    image.save(NEW_IMAGE_NAME)
+
+
+def create_image_from_data(image_data, image_width, image_height):
+    image = Image.new('RGB', (image_width, image_height))
+    image.putdata(image_data)
+    return image
+
+
+def main():
+    with open(ASCII_ART_FILE_NAME, "r") as ascii_art_file:
+        new_image_width, new_image_height = get_file_dimentions(ascii_art_file)
+        new_image_data = convert_ascii_art_to_image_data(ascii_art_file, new_image_width)
+        new_image = create_image_from_data(new_image_data, new_image_width, new_image_height)
+        save_image(new_image)
+
+
+if __name__ == '__main__':
+    main()
